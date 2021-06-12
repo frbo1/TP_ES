763521f6196d0896d4bb5a5d84dcfdcef476131d
Filipe <filipe.presentations@aleeas.com>
2021-06-12 00:32:35 -0300

    Adicionando formulário para avaliação de didática e da relação com os alunos

--- gerar_pagina.py
+++ gerar_pagina.py
@@ -6,6 +6,7 @@ def gerar_professores(pagina):
     # Os ultimos elementos das listas são \n, por isso nós pegamos até o penúltimo.
     nomes = nomes_arq.read().split('\n')[:-1]
     fotos = fotos_arq.read().split('\n')[:-1]
+    conteudo_interativo = open("templates/index-interativo.html").read()
 
     codigo = ""
     for i in range(0, len(nomes)):
@@ -15,24 +16,12 @@ def gerar_professores(pagina):
         snippet_nome = 2 * '\t' + "<p> " + nomes[i] + "</p>\n"
         snippet_foto = 2 * '\t' + "<img src='" + caminho_foto + "' alt='Foto de perfil do professor'>\n" 
 
-        codigo += snippet_item + snippet_nome + snippet_foto + gerar_conteudo_interativo() +  snippet_item_f
+        codigo += snippet_item + snippet_nome + snippet_foto + conteudo_interativo + snippet_item_f
 
     nomes_arq.close()
     fotos_arq.close()
 
     pagina.write(codigo)
-
-def gerar_conteudo_interativo():
-
-    snippet_didatica = "<p> Didática: </p>\n"
-    snippet_relacao = "<p> Relação com os alunos: </p>\n"
-    snippet_form = ''' 
-		    <form>
-			<textarea name="'textarea'" rows=5 cols=100> Escreva seu comentário aqui! </textarea>
-			<button type="submit"> Enviar comentário. </button>
-		    </form>
-		   '''
-    return snippet_form + snippet_didatica + snippet_relacao 
 
 def gerar_inicio_da_pagina(pagina):
     
