12c8935e86d017cc291b9c86d9356b8c23fe420a
jacksonns <jackson.guima2@gmail.com>
2021-06-17 00:20:14 -0300

    Atualizando o Banco de Dados

--- resources/db.py
+++ resources/db.py
@@ -1,15 +1,19 @@
 import sqlite3
 import csv
 
+#Código para popular tabelas do database (Código não precisa ser utilizado novamente)
+#Este código serve apenas para mostrar o processo.
+
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS professores(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
 name TEXT, photo BLOB)")
 
+#Script para popular database com info. dos professores
+"""
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
-    line_count = 0
     for row in csv_reader:
         # row[0] : nome do professor
         # row[1] : photo url
@@ -17,9 +21,35 @@ with open('data.csv') as csv_file:
         url = "C:\\Users\\User\\Documents\\TP_ES\\resources\\fotos-professores\\" + row[1]
         with open(url, 'rb') as f:
             photo = f.read()
-        cursor.execute(""" INSERT INTO professores (name, photo) VALUES (?,?)""", (name,photo))
+            #cursor.execute...
         conn.commit()
+"""
+#cursor.execute(""" INSERT INTO professores (name, photo) VALUES (?,?)""", (name,photo))
 
+cursor.execute("CREATE TABLE IF NOT EXISTS comentarios(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \
+prof_id REFERENCES professores(id), comment TEXT)")
+conn.commit()
+
+#Script para popular tabela de comentários
+"""
+with open('C:\\Users\\User\\Documents\\TP_ES\\resources\\data2.txt') as file:
+    for row in file:
+        # row[0] : nome do professor
+        # row[1] : photo url
+        # row[2] ... row[n] : comentários
+        row = row.split(';')
+        n = len(row)
+        name = row[0]
+        #prof_id = ...
+        prof_id = prof_id.fetchone()[0]
+        for i in range(2, n):
+            if i == (n-1):
+                row[i] = row[i].rstrip('\n')
+            #cursor.execute...
+            conn.commit()
+"""
+#prof_id = cursor.execute(""" SELECT id FROM professores WHERE name = (?) """, (name,))
+#cursor.execute(""" INSERT INTO comentarios (prof_id,comment) VALUES (?,?) """, (prof_id,str(row[i])))
 
 cursor.close()
 conn.close()
