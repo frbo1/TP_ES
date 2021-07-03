b9bad3642fef41ed22d1b15dac5d90753b49701d
memorieslp <42478443+memorieslp@users.noreply.github.com>
2021-07-03 16:19:26 -0300

    Reorganizacao das pastas, e adicionando informacao para os professores

---
+++ TP_ES/planilhao/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import DetailView, ListView
+from django.shortcuts import render
+
+from .models import Professores, Comentarios
+
+def home(request):
+    data = Professores.objects.all()
+    return render(request, "index.html", {"profs":data})
+ 
+
+def detalhe_professor(request, slug):
+    data = Professores.objects.get(slug=slug)
+    data2 = Comentarios.objects.all()
+    return render(request, "detalhe_professor.html", {"prof":data, "coment":data2})
--- resources/mergefile.py
+++
@@ -1,8 +0,0 @@
-names = [line.rstrip() for line in open(r'C:\Users\User\Documents\TP_ES\resources\nomes_professores.txt',encoding="utf-8")]
-img = [line.rstrip() for line in open(r'C:\Users\User\Documents\TP_ES\resources\urls_fotos',encoding="utf-8")]
-
-file = open('data.csv', 'w')
-for i in range (len(names)):
-    file.write(names[i] + ';' + img[i] + '\n')
-
-file.close()
